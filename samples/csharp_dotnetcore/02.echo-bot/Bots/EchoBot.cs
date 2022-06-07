c39f19972e5626d5b6b485cf05a9c5eb9e2b7e73
sw-joelmut <joel.mut@southworks.com>
2022-06-07 16:24:35 -0300

    fix

--- samples/csharp_dotnetcore/02.echo-bot/Bots/EchoBot.cs
+++ samples/csharp_dotnetcore/02.echo-bot/Bots/EchoBot.cs
@@ -14,23 +14,19 @@ namespace Microsoft.BotBuilderSamples.Bots
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken)
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText =  "Hello and welcome!"
+            var welcomeText = "Hello and welcome!"
             foreach (var member in membersAdded)
-
             {
-
-                  if (member.Id != turnContext.Activity.Recipient.Id)
+                if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-
-                    };
-
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
                 }
+            }
         }
     }
 }
