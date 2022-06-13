761d8069796543dda4e0b39af7c9d75929b9dec7
Emiliano Quiroga <emiliano.quiroga@7-11.com>
2022-06-13 10:46:33 -0300

    [AdaptiveCardsBot] Errors fixed.

--- samples/csharp_dotnetcore/07.using-adaptive-cards/Program.cs
+++ samples/csharp_dotnetcore/07.using-adaptive-cards/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Microsoft.BotBuilderSamples
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
