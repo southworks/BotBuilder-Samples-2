9f972e49b6936fd933a872b38361abc019371bb2
Emiliano Quiroga <emiliano.quiroga@7-11.com>
2022-06-16 16:36:00 -0300

    [TeamsLinkUnfurling] Errors fixed

--- samples/csharp_dotnetcore/55.teams-link-unfurling/Controllers/BotController.cs
+++ samples/csharp_dotnetcore/55.teams-link-unfurling/Controllers/BotController.cs
@@ -28,6 +28,6 @@ namespace Microsoft.BotBuilderSamples.Controllers
         public async Task PostAsync() =>
             // Delegate the processing of the HTTP POST to the adapter.
             // The adapter will invoke the bot.
-            await _adapter.ProcessAsync(Request, Response, _bot);
+            await _adapter.ProcessAsync(Request, Response, _bot).ConfigureAwait(false);
     }
 }
